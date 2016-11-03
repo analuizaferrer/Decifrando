1fa5f1967208d40d3c2955f77ccdbe993d35db2f
Ana Luiza Ferrer <analcferrer@gmail.com>
2016-11-03 19:02:30 -0200

    letters

---
+++ Decifrando/Letter.swift
@@ -0,0 +1,53 @@
+//
+//  Letter.swift
+//  Decifrando
+//
+//  Created by Ana Luiza Ferrer on 03/11/16.
+//
+//
+
+import Foundation
+import SpriteKit
+
+class Letter: SKLabelNode {
+    
+    init (letter: Character) {
+        
+        super.init(fontNamed: "Arial Rounded MT Bold")
+        self.text = "\(letter)"
+        self.fontSize = 100
+        self.fontColor = SKColor.black
+        
+    }
+    
+    override init() {
+        
+        super.init()
+        self.fontName = "Arial Rounded MT Bold"
+        self.text = randomString(length: 1)
+        self.fontSize = 100
+        self.fontColor = SKColor.black
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func randomString(length: Int) -> String {
+        
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyz"
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
+    }
+    
+}
--- Decifrando/LevelScene.swift
+++ Decifrando/LevelScene.swift
@@ -12,11 +12,13 @@ import SpriteKit
 class LevelScene: SKScene {
     
     let correctWord = "pato"
-    
     var boxArray: [Box]!
+    var lettersArray: [SKLabelNode]!
     
     override func didMove(to view: SKView) {
+        
         self.backgroundColor = UIColor.blue
+        
         boxArray = []
         
         for letter in correctWord.characters {
@@ -26,7 +28,6 @@ class LevelScene: SKScene {
         }
         
         var x: CGFloat = 30
-        
         for box in boxArray {
             
             box.position = CGPoint(x: x, y: size.height/2)
@@ -35,8 +36,30 @@ class LevelScene: SKScene {
             x = x + box.size.width + 10
         }
         
+        lettersArray = []
+        
+        for letter in correctWord.characters {
+            
+            lettersArray.append(Letter(letter: letter))
+            
+        }
+        
+        for _ in 0 ..< 10-correctWord.characters.count {
+        
+            lettersArray.append(Letter())
+        
+        }
+        
+        var x2: CGFloat = 30
+        for letter in lettersArray {
+            
+            letter.position = CGPoint(x: x2, y: size.height/4)
+            addChild(letter)
+            
+            x2 = x2 + 50
+            
+        }
+        
     }
-    
-   
     
 }
